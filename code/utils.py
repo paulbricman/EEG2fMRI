7b1b6709fc01c087620542483bc6a43c80a86986
paubric <paubric@gmail.com>
2020-12-05 15:31:28 +0100

    Update comments

--- code/utils.py
+++ code/utils.py
@@ -5,6 +5,7 @@ from scipy.io import loadmat
 
 import os
 
+
 class OddballDataset(Dataset):
     """Auditory and Visual Oddball EEG-fMRI dataset."""
 
@@ -19,32 +20,39 @@ class OddballDataset(Dataset):
         self.root_dir = root_dir
         self.transform = transform
 
+        # Setting constants based on scanner specifications
         self.samples_per_trial = 155
         self.trials_per_subject = 6
         self.samples_per_subject = self.samples_per_trial * self.trials_per_subject
         self.subject_count = len(os.listdir())
         self.skipped_samples_per_trial = 170 - self.samples_per_trial
-        self.eeg_frequency = 1000
-        self.fmri_period = 2
-        self.skipped_eeg_per_trial = self.skipped_samples_per_trial * self.fmri_period * self.eeg_frequency
+        self.eeg_frequency = 1000 # Hz
+        self.fmri_period = 2 # seconds
+        self.skipped_eeg_per_trial = self.skipped_samples_per_trial * \
+            self.fmri_period * self.eeg_frequency
         self.eeg_sample_step = self.fmri_period * self.eeg_frequency
-        self.sample_length = 30
+        self.sample_length = 30 # seconds
         self.eeg_sample_length = self.eeg_frequency * self.sample_length
 
     def __len__(self):
         return self.subject_count * self.trials_per_subject * self.samples_per_trial
 
     def __getitem__(self, idx):
+        # Computing subject, trial, and trial-level sample index of sample
         subject = idx // self.samples_per_subject
         trial = idx % self.samples_per_subject // self.samples_per_trial
         sample_in_trial = idx % self.samples_per_trial
 
+        # Computing sample data location in trial recording
         eeg_start_index = sample_in_trial * self.eeg_sample_step
         eeg_end_index = eeg_start_index + self.eeg_sample_length
         fmri_index = sample_in_trial + self.skipped_samples_per_trial
 
-        subject_path = self.root_dir + '/ds116_sub' + f'{(subject + 1):03}' + '/sub' + f'{(subject + 1):03}/'
-        trial_path = 'task' + f'{(trial // 3 + 1):03}' + '_run' + f'{(trial % 3 + 1):03}/'
+        # Loading EEG and fMRI data from sample trial
+        subject_path = self.root_dir + '/ds116_sub' + \
+            f'{(subject + 1):03}' + '/sub' + f'{(subject + 1):03}/'
+        trial_path = 'task' + f'{(trial // 3 + 1):03}' + \
+            '_run' + f'{(trial % 3 + 1):03}/'
         eeg_path = subject_path + 'EEG/' + trial_path + 'EEG_rereferenced.mat'
         fmri_path = subject_path + 'BOLD/' + trial_path + 'bold_mcf_brain.nii.gz'
 
@@ -52,6 +60,7 @@ class OddballDataset(Dataset):
         fmri_trial_data = nib.load(fmri_path).get_fdata()
 
         return [eeg_trial_data.shape, fmri_trial_data.shape]
+
 
 dataset = OddballDataset('../../OddballData')
 print(dataset[600])
