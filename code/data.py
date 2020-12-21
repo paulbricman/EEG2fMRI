37f5db7ee35c1b17b5d2b201889ac992dd05b1ad
paubric <paubric@gmail.com>
2020-12-21 19:28:24 +0200

    Update data.py replacing trial with block

--- code/data.py
+++ code/data.py
@@ -24,51 +24,51 @@ class OddballDataset(Dataset):
         self.transform = transform
 
         # Setting constants based on scanner specifications
-        self.samples_per_trial = 155
-        self.trials_per_subject = 6
-        self.samples_per_subject = self.samples_per_trial * self.trials_per_subject
+        self.samples_per_block = 155
+        self.blocks_per_subject = 6
+        self.samples_per_subject = self.samples_per_block * self.blocks_per_subject
         self.subject_count = len(os.listdir())
-        self.skipped_samples_per_trial = 170 - self.samples_per_trial
+        self.skipped_samples_per_block = 170 - self.samples_per_block
         self.eeg_frequency = 1000  # Hz
         self.fmri_period = 2  # seconds
-        self.skipped_eeg_per_trial = self.skipped_samples_per_trial * \
+        self.skipped_eeg_per_block = self.skipped_samples_per_block * \
             self.fmri_period * self.eeg_frequency
         self.eeg_sample_step = self.fmri_period * self.eeg_frequency
         self.sample_length = 30  # seconds
         self.eeg_sample_length = self.eeg_frequency * self.sample_length
 
     def __len__(self):
-        return self.subject_count * self.trials_per_subject * self.samples_per_trial
+        return self.subject_count * self.blocks_per_subject * self.samples_per_block
         # first fMRI volume at t=2s
 
     def __getitem__(self, idx):
-        # Computing subject, trial, and trial-level sample index of sample
+        # Computing subject, block, and block-level sample index of sample
         subject = idx // self.samples_per_subject
-        trial = idx % self.samples_per_subject // self.samples_per_trial
-        sample_in_trial = idx % self.samples_per_trial
+        block = idx % self.samples_per_subject // self.samples_per_block
+        sample_in_block = idx % self.samples_per_block
 
-        # Computing sample data location in trial recording
-        eeg_start_index = sample_in_trial * self.eeg_sample_step
+        # Computing sample data location in block recording
+        eeg_start_index = sample_in_block * self.eeg_sample_step
         eeg_end_index = eeg_start_index + self.eeg_sample_length
-        fmri_index = sample_in_trial + self.skipped_samples_per_trial
+        fmri_index = sample_in_block + self.skipped_samples_per_block
 
-        # Loading EEG and fMRI data from sample trial
+        # Loading EEG and fMRI data from sample block
         subject_path = self.root_dir + '/sub' + f'{(subject + 1):03}/'
-        trial_path = 'task' + f'{(trial // 3 + 1):03}' + \
-            '_run' + f'{(trial % 3 + 1):03}/'
-        eeg_path = subject_path + 'EEG/' + trial_path + 'EEG_rereferenced.mat'
-        fmri_path = subject_path + 'BOLD/' + trial_path + 'bold_mcf_brain.nii.gz'
+        block_path = 'task' + f'{(block // 3 + 1):03}' + \
+            '_run' + f'{(block % 3 + 1):03}/'
+        eeg_path = subject_path + 'EEG/' + block_path + 'EEG_rereferenced.mat'
+        fmri_path = subject_path + 'BOLD/' + block_path + 'bold_mcf_brain.nii.gz'
 
-        eeg_trial_data = loadmat(eeg_path)['data_reref']
-        fmri_trial_data = nib.load(fmri_path).get_fdata()
+        eeg_block_data = loadmat(eeg_path)['data_reref']
+        fmri_block_data = nib.load(fmri_path).get_fdata()
 
-        # Extract relevant sample data from trial
-        eeg_trial_data = eeg_trial_data[:34]
-        fmri_trial_data = np.moveaxis(fmri_trial_data, -1, 0)
+        # Extract relevant sample data from block
+        eeg_block_data = eeg_block_data[:34]
+        fmri_block_data = np.moveaxis(fmri_block_data, -1, 0)
 
         eeg_sample_data = np.array(
-            [eeg_trial_data[channel][eeg_start_index:eeg_end_index] for channel in range(len(eeg_trial_data))])
-        fmri_sample_data = fmri_trial_data[fmri_index]
+            [eeg_block_data[channel][eeg_start_index:eeg_end_index] for channel in range(len(eeg_block_data))])
+        fmri_sample_data = fmri_block_data[fmri_index]
 
         # Roughly standardize EEG and fMRI sample data
         eeg_sample_data = eeg_sample_data / 30
