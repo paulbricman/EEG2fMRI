e54509ae45ca4ec80900a752e06b34dd43fc8f29
paubric <paubric@gmail.com>
2020-12-22 16:40:31 +0200

    Update model.py train.py

--- code/model.py
+++ code/model.py
@@ -2,29 +2,58 @@ import torch
 import pytorch_lightning as pl
 from torch import nn
 import torch.nn.functional as F
-
+import numpy as np
 
 class ConvolutionalModel(pl.LightningModule):
 
     def __init__(self):
         super().__init__()
+        self.eeg_frequency = 1000  # Hz
+        self.sample_length = 30  # seconds
+        self.eeg_channels = 34
+        self.eeg_sample_length = self.eeg_frequency * self.sample_length
+        self.fmri_dimensions = [64, 64, 32]
+
         self.model = nn.Sequential(
-            nn.Conv2d(1, 8, (1, 5), stride=16),
-            nn.AvgPool2d((1, 64)),
+            nn.Conv2d(1, 4, (1, 5), stride=2),
+            nn.AvgPool2d((1, 2)),
+            nn.ReLU(),
+            nn.Conv2d(4, 8, (1, 5), stride=2),
+            nn.AvgPool2d((1, 2)),
+            nn.ReLU(),
+            nn.Conv2d(8, 16, (1, 5), stride=2),
+            nn.AvgPool2d((1, 2)),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, (1, 5), stride=2),
+            nn.AvgPool2d((1, 2)),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, (1, 5), stride=2),
+            nn.AvgPool2d((1, 2)),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, (1, 5), stride=2),
+            nn.AvgPool2d((1, 2)),
+            nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(696, 32 * 64 * 64)
-        )
+            nn.Linear(768, np.prod(self.fmri_dimensions))
+        ).double()
 
     def forward(self, x):
-        return self.model(x)
+        x = x.view(x.size(0), 1, self.eeg_channels, self.eeg_sample_length)
+        y_pred = self.model(x)
+        y_pred = y_pred.view(y_pred.size(0), *self.fmri_dimensions)
+        return y_pred
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        x = x.view(x.size(0), 1, 34, 30000)
-        y_pred = self.model(x)
-        y_pred = y_pred.view(y_pred.size(0), 64, 64, 32)
-        loss = F.mse_loss(y, y_pred)
+        loss = F.mse_loss(y, self.forward(x))
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        loss = F.mse_loss(y, self.forward(x))
+        self.log('val_loss', loss)
         return loss
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=0.02)
+        return torch.optim.Adam(self.parameters())
--- code/train.py
+++ code/train.py
@@ -1,13 +1,20 @@
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
+import torch
 from data import OddballDataset
 from model import ConvolutionalModel
 
 dataset = OddballDataset('../../OddballData')
 
-train_loader = DataLoader(dataset)
+train_prop, test_prop = 0.8, 0.1
+train_size = int(train_prop * len(dataset))
+test_size = int(test_prop * len(dataset))
+val_size = len(dataset) - train_size - test_size
+
+train_dataset, test_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, test_size, val_size])
+train_loader = DataLoader(train_dataset)
+val_loader = DataLoader(val_dataset)
 
 model = ConvolutionalModel()
-
 trainer = pl.Trainer()
-trainer.fit(model, train_loader)
+trainer.fit(model, train_loader, val_loader)
