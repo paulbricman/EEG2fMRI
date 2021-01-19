f9eb5eafbd33a60d017fa714e5ea2637430fa239
paubric <paubric@gmail.com>
2021-01-19 17:01:39 +0100

    Update model.py with structure for transformer model

--- code/model.py
+++ code/model.py
@@ -14,22 +14,31 @@ class TransformerModel(pl.LightningModule):
         self.eeg_channels = 34
         self.eeg_sample_length = self.eeg_frequency * self.sample_length
         self.fmri_dimensions = [53, 63, 52]
+        self.flattened_slice_length = 63 * 52
+
+        # Start of sequence embedding
+        self.sos = nn.Embedding(1, self.flattened_slice_length).double()
 
         # Encoder for generating channel embeddings from channel recordings
-        self.encoder = nn.Linear(30000, 512).double()
+        self.encoder = nn.Linear(30000, self.flattened_slice_length).double()
 
-        # Transformer for generating slice embeddings from channel embeddings
-        self.transformer = nn.Transformer().double()
+        # Transformer for generating slices from channel embeddings
+        self.transformer = nn.Transformer(self.flattened_slice_length).double()
 
-        # Decoder for generating slices from slice embeddings
-        self.decoder = nn.Linear(512, 63 * 52)
-
-    def forward(self, x):
-        channel_recordings = x.view(x.size(0), 1, self.eeg_channels, self.eeg_sample_length)
+    def forward(self, x, y):
+        batch_size = x.size(0)
+        channel_recordings = x.view(batch_size * self.eeg_channels, self.eeg_sample_length)
 
         # Generate channel embeddings from channel recordings
-        # Generate slice embeddings from channel embeddings
-        # Generate slices from slice embeddings
+        channel_embeddings = self.encoder(channel_recordings)
+        channel_embeddings = channel_embeddings.view(self.eeg_channels, batch_size, self.flattened_slice_length)
+
+        # TODO apply positional embeddings
+        # Generate slices from channel embeddings
+        tgt = y.view(53, batch_size, self.flattened_slice_length)
+        tgt = torch.cat((self.sos, tgt[:-1]))
+        mask = (torch.triu(torch.ones(53, 53)) == 1).transpose(0, 1)
+        slices = self.transformer(channel_embeddings, tgt, tgt_mask = mask)
         
 
     def training_step(self, batch, batch_idx):
