5fa096fcfb9dc68b4b2ad681dc07216e07a542f3
paubric <paubric@gmail.com>
2021-01-20 12:55:18 +0100

    Update model.py with coherent forward until pre-transformer reshape

--- code/model.py
+++ code/model.py
@@ -14,25 +14,30 @@ class TransformerModel(pl.LightningModule):
         self.channel_pos_emb = nn.Embedding(34, 63 * 52).double()
         self.slice_pos_emb = nn.Embedding(53, 63 * 52).double()
 
+        self.register_buffer('sos_emb_idx', torch.LongTensor([0]))
+        self.register_buffer('channel_pos_emb_idx', torch.arange(0, 34, dtype=torch.long))
+        self.register_buffer('slice_pos_emb_idx', torch.arange(0, 53, dtype=torch.long))
+
         self.encoder = nn.Linear(7500, 63 * 52).double()
-        self.transformer = nn.Transformer(63 * 52).double()
+        self.transformer = nn.Transformer(63 * 52, nhead = 4, \
+		num_encoder_layers = 2, num_decoder_layers = 2).double()
 
     def forward(self, x, y):
         # Downsample to 250 Hz
-        x = nn.AvgPool2d(x, (1, 4))
+        x = nn.AvgPool2d((1, 4))(x) # N, 34, 7500
 
         # Generate channel embeddings from channel recordings
-        channel_emb = torch.cat([self.encoder(sample) for sample in x]) # N, S, E
+        channel_emb = torch.cat([self.encoder(sample) for sample in x]).view(x.size(0), 34, 63 * 52)
 
         # Apply channel positional embeddings
-        channel_emb = torch.cat([sample + self.channel_pos_emb for sample in channel_emb]) # N, S, E
+        channel_emb = torch.cat([sample + self.channel_pos_emb(self.channel_pos_emb_idx) for sample in channel_emb]).view(x.size(0), 34, 63 * 52)
 
         # Shift slices in target
         slices = y.view(y.size(0), 53, 63 * 52)
-        slices = torch.cat([torch.cat((self.sos_emb, sample[:-1])) for sample in slices])
+        slices = torch.cat([torch.cat((self.sos_emb(self.sos_emb_idx), sample[:-1])) for sample in slices]).view(y.size(0), 53, 63 * 52)
 
         # Apply slice positional embeddings
-        slices = torch.cat([sample + self.slice_pos_emb for sample in slices]) # N, T, E
+        slices = torch.cat([sample + self.slice_pos_emb(self.slice_pos_emb_idx) for sample in slices]).view(y.size(0), 53, 63 * 52)
 
         # Apply slice positional embeddings
         channel_emb = torch.moveaxis(channel_emb, 1, 0)
