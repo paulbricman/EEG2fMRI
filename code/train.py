626444287d84923eefd71b747cc68eb6ce422322
Paul Bricman <paulbricman@protonmail.com>
2021-02-10 17:51:34 +0200

    Update train.py

--- code/train.py
+++ code/train.py
@@ -1,12 +1,8 @@
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 import torch
-from data import OddballDataset, fmri_preview
+from data import OddballDataset
 from model import ConvolutionalModel
-import numpy as np
-
-trained_model = ConvolutionalModel.load_from_checkpoint('./trained.ckpt')
-trained_model.eval()
 
 dataset = OddballDataset('../../OddballData')
 
@@ -15,18 +11,11 @@ train_size = int(train_prop * len(dataset))
 test_size = int(test_prop * len(dataset))
 val_size = len(dataset) - train_size - test_size
 
-batch_size = 8
+batch_size = 128
 train_dataset, test_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, test_size, val_size], generator=torch.Generator().manual_seed(0))
-train_loader = DataLoader(train_dataset, num_workers=8, batch_size=batch_size, drop_last=True)
-val_loader = DataLoader(val_dataset, num_workers=8, batch_size=batch_size, drop_last=True)
+train_loader = DataLoader(train_dataset, num_workers=32, batch_size=batch_size, shuffle=True, drop_last=True)
+val_loader = DataLoader(val_dataset, num_workers=32, batch_size=batch_size, drop_last=True)
 
-for i_batch, sample_batched in enumerate(val_loader):
-	if i_batch == 0:
-		pred = trained_model(sample_batched[0])
-
-		for i in range(batch_size):
-			fmri_preview(torch.mul(sample_batched[1][0][i], sample_batched[1][1][i]).detach().numpy())
-			fmri_preview(torch.mul(pred[i], sample_batched[1][1][i]).detach().numpy())
-
-	break
-
+model = ConvolutionalModel()
+trainer = pl.Trainer(gpus=4, accelerator='dp', max_epochs=1, track_grad_norm=2, check_val_every_n_epoch=5, gradient_clip_val=0.1)
+trainer.fit(model, train_loader, val_loader)
