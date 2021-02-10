11801e90d5334afd31f5bf4ca47141e33084c3d4
Paul Bricman <paubric@gmail.com>
2021-02-10 17:51:02 +0200

    Update train.py

--- code/train.py
+++ code/train.py
@@ -1,8 +1,12 @@
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 import torch
-from data import OddballDataset
-from model import TransformerModel
+from data import OddballDataset, fmri_preview
+from model import ConvolutionalModel
+import numpy as np
+
+trained_model = ConvolutionalModel.load_from_checkpoint('./trained.ckpt')
+trained_model.eval()
 
 dataset = OddballDataset('../../OddballData')
 
@@ -11,11 +15,18 @@ train_size = int(train_prop * len(dataset))
 test_size = int(test_prop * len(dataset))
 val_size = len(dataset) - train_size - test_size
 
-batch_size = 32
-train_dataset, test_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, test_size, val_size])
-train_loader = DataLoader(train_dataset, num_workers=32, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, num_workers=32, batch_size=batch_size)
+batch_size = 8
+train_dataset, test_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, test_size, val_size], generator=torch.Generator().manual_seed(0))
+train_loader = DataLoader(train_dataset, num_workers=8, batch_size=batch_size, drop_last=True)
+val_loader = DataLoader(val_dataset, num_workers=8, batch_size=batch_size, drop_last=True)
 
-model = TransformerModel()
-trainer = pl.Trainer(gpus=[3], max_epochs=50)
-trainer.fit(model, train_loader, val_loader)
+for i_batch, sample_batched in enumerate(val_loader):
+	if i_batch == 0:
+		pred = trained_model(sample_batched[0])
+
+		for i in range(batch_size):
+			fmri_preview(torch.mul(sample_batched[1][0][i], sample_batched[1][1][i]).detach().numpy())
+			fmri_preview(torch.mul(pred[i], sample_batched[1][1][i]).detach().numpy())
+
+	break
+
