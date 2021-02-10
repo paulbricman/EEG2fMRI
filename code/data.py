6a48397b482d9bfd33a20c1fe8ad64ef746ab1c9
Paul Bricman <paulbricman@protonmail.com>
2021-02-10 18:35:49 +0200

    Update data.py

--- code/data.py
+++ code/data.py
@@ -12,63 +12,45 @@ import matplotlib.pyplot as plt
 class OddballDataset(Dataset):
     """Auditory and Visual Oddball EEG-fMRI dataset."""
 
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, subj_sel):
         """
         Args:
             root_dir (string): Path to the root folder containing subject subfolders.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
+            subj_sel (list): Selection of subjects to be considered in an instance.
         """
 
         self.root_dir = root_dir
-        self.transform = transform
-
-        # Setting constants based on scanner specifications
-        self.samples_per_block = 155
-        self.blocks_per_subject = 6
-        self.samples_per_subject = self.samples_per_block * self.blocks_per_subject
-        self.subject_count = 16
-        self.skipped_samples_per_block = 170 - self.samples_per_block
-        self.eeg_frequency = 1000  # Hz
-        self.fmri_period = 2  # seconds
-        self.skipped_eeg_per_block = self.skipped_samples_per_block * \
-            self.fmri_period * self.eeg_frequency
-        self.eeg_sample_step = self.fmri_period * self.eeg_frequency
-        self.sample_length = 30  # seconds
-        self.eeg_sample_length = self.eeg_frequency * self.sample_length
+        self.subj_sel = subj_sel
 
     def __len__(self):
-        return self.subject_count * self.blocks_per_subject * self.samples_per_block
-        # first fMRI volume at t=2s
+        return len(self.subj_sel) * 6 * 155
 
     def __getitem__(self, idx):
         # Computing subject, block, and block-level sample index of sample
-        subject = idx // self.samples_per_subject
-        block = idx % self.samples_per_subject // self.samples_per_block
-        sample_in_block = idx % self.samples_per_block
-
-        if subject >= 3:
-            subject += 1
+        subj_sel_idx = idx // (6 * 155)
+        subj_idx = self.subj_sel[subj_sel_idx]
+        block_idx = (idx % (6 * 155)) // 155
+        sample_idx = idx % 155
 
         # Computing sample data location in block recording
-        eeg_start_index = sample_in_block * self.eeg_sample_step
-        eeg_end_index = eeg_start_index + self.eeg_sample_length
-        fmri_index = sample_in_block + self.skipped_samples_per_block
+        eeg_start_idx = sample_idx * 2000
+        eeg_end_idx = eeg_start_idx + 30000
+        fmri_idx = sample_idx + 15
 
         # Loading EEG and fMRI data from sample block
-        subject_path = self.root_dir + '/sub' + f'{(subject + 1):03}/'
-        eeg_path = subject_path + 'EEG/' + 'task' + f'{(block // 3 + 1):03}' + \
-            '_run' + f'{(block % 3 + 1):03}/' + 'EEG_rereferenced.mat'
-        mask_path = subject_path + 'BOLD/mask.nii'
+        subj_path = self.root_dir + '/sub' + f'{subj_idx:03}/'
+        eeg_path = subj_path + 'EEG/' + 'task' + f'{(block_idx // 3 + 1):03}' + \
+            '_run' + f'{(block_idx % 3 + 1):03}/' + 'EEG_rereferenced.mat'
+        mask_path = subj_path + 'BOLD/mask.nii'
 
-        if block < 3:
+        if block_idx < 3:
             block_type = 'auditory'
         else:
             block_type = 'visual'
 	    
-        fmri_path = subject_path + 'BOLD/swarsub-' + f'{(subject + 1):02}_task-' + \
+        fmri_path = subj_path + 'BOLD/swarsub-' + f'{subj_idx:02}_task-' + \
             block_type + 'oddballwithbuttonresponsetotargetstimuli_run-' + \
-            f'{(block % 3 + 1):02}_' + 'bold.nii'
+            f'{(block_idx % 3 + 1):02}_' + 'bold.nii'
 
         eeg_block_data = loadmat(eeg_path)['data_reref']
         fmri_block_data = nib.load(fmri_path).get_fdata()
@@ -79,20 +61,17 @@ class OddballDataset(Dataset):
         fmri_block_data = np.moveaxis(fmri_block_data, -1, 0)
 
         eeg_sample_data = np.array(
-            [eeg_block_data[channel][eeg_start_index:eeg_end_index] for channel in range(len(eeg_block_data))])
-        fmri_sample_data = fmri_block_data[fmri_index]
+            [eeg_block_data[channel][eeg_start_idx:eeg_end_idx] for channel in range(len(eeg_block_data))])
+        fmri_sample_data = fmri_block_data[fmri_idx]
 
         # Standardize EEG and fMRI sample data using pre-computed values
         eeg_sample_data = (eeg_sample_data + 2286) / 4200
         fmri_sample_data = (fmri_sample_data + 419) / 4555
-        #fmri_sample_data += 500
-        #fmri_sample_data = (np.log10(fmri_sample_data) - 1.4) / 3.76
-        #fmri_sample_data = fmri_sample_data * 2 - 1
 
         # Convert to Pytorch tensors
         eeg_sample_data = torch.from_numpy(eeg_sample_data.astype('float32'))
         fmri_sample_data = torch.from_numpy(fmri_sample_data.astype('float32'))
-        subject = torch.from_numpy(np.array([subject / 100]).astype('float32'))
+        subject = torch.from_numpy(np.array([subj_idx / 100]).astype('float32'))
         mask_data = torch.from_numpy(mask_data)
 
         return [[eeg_sample_data, subject], [fmri_sample_data, mask_data]]
